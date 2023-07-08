a4b1ac5417598b78e9016ec2cd6d10765700f15d
swethabijanapalli <swethabijanapalli@gmail.com>
2023-07-08 19:33:29 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PomodoroClock from './PomodoroClock';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='main'>
+      <PomodoroClock />
+    </div>
+  );
+};
+
+export default App;
